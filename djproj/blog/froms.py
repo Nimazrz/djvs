f3106cfefbf914b77910b941632a6b4bdcfbaff4
Nima <Nimaaa8413@gmail.com>
2024-04-01 03:06:24 +0330

    done

--- djproj/blog/froms.py
+++ djproj/blog/froms.py
@@ -2,15 +2,15 @@ from django import forms
 
 
 class TicketForm(forms.Form):
-    # چون توی تمپلیت وارد کردیم اینجا دیگه بی معنیه و میتوانیم وارد نکنیم
-    SUBJECT_CHOICES = (
-        ('پیشنهاد', 'پیشنهاد'),
-        ('انتقاد', 'انتقاد'),
-        ('گزارش', 'گزارش'),
-    )
+    # چون توی تمپلیت وارد کردیم اینجا دیگه بی معنیه و میتوانیم وارد نکنیم و در سابچکت بجای چوز فیلد باید از چر فیلد استفاده کنیم  و پرانتز روبروش رو خالی کنیم
+    # SUBJECT_CHOICES = (
+    #     ('پیشنهاد', 'پیشنهاد'),
+    #     ('انتقاد', 'انتقاد'),
+    #     ('گزارش', 'گزارش'),
+    # )
 
     message = forms.CharField(widget=forms.Textarea, required=True)
     name = forms.CharField(max_length=250 ,required=True)
     email = forms.EmailField()
     phone = forms.CharField(max_length=11,required=True)
-    subject = forms.CharField(choices=SUBJECT_CHOICES)
+    subject = forms.CharField()#choices=SUBJECT_CHOICES
