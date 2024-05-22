0b1a1f9a93ef8540dfc0a9374c451ed11ceffefe
Nima <Nimaaa8413@gmail.com>
2024-05-23 01:26:33 +0330

    idk

--- djproj/blog/urls.py
+++ djproj/blog/urls.py
@@ -37,11 +37,9 @@ urlpatterns=[
     # path('login/', views.user_login, name='login'), 
 
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     path('password-change/', auth_views.PasswordChangeView.as_view(success_url='done'), name='password_change'),
-
     path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
     path('password-reset/', auth_views.PasswordResetView.as_view(success_url='done'), name='password_reset'),
